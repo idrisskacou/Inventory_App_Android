b30a08e64a91fd8ec41d7969338f2b506ef29f8d
Idriss Kacou <Idrisskacou@outlook.com>
2024-03-21 22:36:51 -0700

    Update Git

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Idriss_Kacou_Opt_1_Inventory_App"
+include(":app")
+ 
